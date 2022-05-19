3833cea1fa25ad5c43e9063307ec3342340f2be0
Cory Nickerson <crnckrsn@gmail.com>
2022-05-19 18:08:32 -0400

    Added Jest Jest still needs some configuration work and a few basic tests to confirm the operation of the client, server and database.

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
-import { createRoot } from 'react-dom/client';
-import React from 'react';
+import { createRoot } from "react-dom/client";
+import React from "react";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 
 function App() {
   return <h1>Hello World</h1>;
