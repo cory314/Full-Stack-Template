efd50113e1ec9ac6cfdbc36999234c6ff30ebbc4
Cory Nickerson <crnckrsn@gmail.com>
2022-05-22 09:31:19 -0400

    ting Please enter the commit message for your changes. Lines starting

--- server/server.js
+++ server/server.js
@@ -3,10 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
+
 const app = express();
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../public/')));
 
-app.listen(3000);
+app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
