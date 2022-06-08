b058ba596e0cf31ee37acd349e02049a08e1ba52
Cory Nickerson <essej4269@Hotmail.com>
2022-06-07 21:33:17 -0400

    nor adjustments for deploy# Please enter the commit message for your changes. Lines starting

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,9 @@ import React from "react";
 const root = createRoot(document.getElementById("root"));
 
 function App() {
-  return <h1>Hello World</h1>;
+  return <><h1>Welcome to CoryNickerson.com</h1>
+  <h3>Soon to be running a full stack tech blog, links to projects and other fun items as I go. Currently 5 weeks from graduating hack reactor and seeking employment. </h3>
+  </>;
 }
 
-root.render(<App />);
+
